e96d0f31be63ef34bfc3db3c99ca064366eaddab
M Matos <github@corp.mmatos.com>
2017-10-03 07:17:33 +0000

    cleaning up ansible repo

--- ansible/cluster.sh
+++
@@ -1,1 +0,0 @@
-oc process -f ./roles/openshift_hosted_templates/files/v1.4/origin/metrics-deployer.yaml    -v HAWKULAR_METRICS_HOSTNAME=hawkular-metrics-openshift-infra.apps.140.86.55.167.xip.io -v CASSANDRA_PV_SIZE=10Gi -v MASTER_URL=https://10.1.1.101:8443 -v IMAGE_VERSION=v1.4.1 | oc create -f -
